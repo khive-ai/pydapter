9fa2f1f38b660093b40500aa15f155837c0c7f63
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2025-07-23 15:55:01 -0400

    Bump version to 1.0.0 and update orjson dependency specification

--- src/pydapter/__init__.py
+++ src/pydapter/__init__.py
@@ -4,16 +4,6 @@ pydapter - tiny trait + adapter toolkit.
 
 from .async_core import AsyncAdaptable, AsyncAdapter, AsyncAdapterRegistry
 from .core import Adaptable, Adapter, AdapterRegistry
-from .fields import (
-    ID,
-    Embedding,
-    Execution,
-    Field,
-    Undefined,
-    UndefinedType,
-    create_model,
-)
-from .protocols import Event, as_event
 
 __all__ = (
     "Adaptable",
@@ -22,15 +12,6 @@ __all__ = (
     "AsyncAdaptable",
     "AsyncAdapter",
     "AsyncAdapterRegistry",
-    "Field",
-    "create_model",
-    "Execution",
-    "Embedding",
-    "ID",
-    "Undefined",
-    "UndefinedType",
-    "Event",
-    "as_event",
 )
 
-__version__ = "0.3.2"
+__version__ = "1.0.0"
